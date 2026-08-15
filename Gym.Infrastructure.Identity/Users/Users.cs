d77ae581441b419167151224988822caa2a6e320
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-08-15 11:46:48 -0400

    git add .     git commit -m "feat(architecture): setup inicial de Clean Architecture y Solution Folders"

--- Gym.Infrastructure.Identity/Users/Users.cs
+++ Gym.Infrastructure.Identity/Users/Users.cs
@@ -11,11 +11,11 @@ namespace Gym.Domain.Entities
         public int UserId { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
-        public string Salt { get; set; }
+        
         public string Role { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
+      
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime LastAccess { get; set; }
